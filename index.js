bbcacf1bc458a05d871d1e3aa02d48d11f0c7bb5
Zachary Dean <Zakedoubled@gmail.com>
2022-07-18 22:48:53 -0400

    commit tree 3650c98d0226bb77c03f6b7dd81faed494554348

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    const cats2 = [...cats, name];
+    return cats2;
+}
+function prependCat(name) {
+    const cats3 = [name, ...cats];
+    return cats3;
+}
+function removeFirstCat() {
+    const cats4 = cats.slice(1);
+    return cats4;
+}
+function removeLastCat() {
+    const cats5 = cats.slice(0, 2);
+    return cats5;
+}
